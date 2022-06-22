dbc1fee74b67ac129ea5b26a7e0513155a7314b7
jekattack <jan.erik.kraus@t-online.de>
2022-06-22 10:53:06 +0200

    init

---
+++ src/main/java/com/github/jekattack/ringedingdong/RingedingdongApplication.java
@@ -0,0 +1,13 @@
+package com.github.jekattack.ringedingdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingedingdongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RingedingdongApplication.class, args);
+    }
+
+}
