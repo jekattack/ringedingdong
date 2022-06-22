ab68f47f831f59ec6164c5ac28b0073aa6ac1202
jekattack <jan.erik.kraus@t-online.de>
2022-06-22 11:23:21 +0200

    surprise added

--- src/main/java/com/github/jekattack/ringedingdong/RingedingdongController.java
+++ src/main/java/com/github/jekattack/ringedingdong/RingedingdongController.java
@@ -8,17 +8,76 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping()
 @RequiredArgsConstructor
 public class RingedingdongController {
 
-    @GetMapping("/{name}")
+    @GetMapping("/{name}/ivebeenrickrolled")
     public String greetingName(@PathVariable String name){
         return "Ringedingdong, " + name + "!";
     }
 
+    @GetMapping("/{name}")
+    public String greetingRick(@PathVariable String name){
+        return "We're no strangers to love\n" +
+                "You know the rules and so do I (do I)\n" +
+                "A full commitment's what I'm thinking of\n" +
+                "You wouldn't get this from any other guy\n" +
+                "I just wanna tell you how I'm feeling\n" +
+                "Gotta make you understand\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n" +
+                "We've known each other for so long\n" +
+                "Your heart's been aching, but you're too shy to say it (say it)\n" +
+                "Inside, we both know what's been going on (going on)\n" +
+                "We know the game and we're gonna play it\n" +
+                "And if you ask me how I'm feeling\n" +
+                "Don't tell me you're too blind to see\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n" +
+                "We've known each other for so long\n" +
+                "Your heart's been aching, but you're too shy to say it (to say it)\n" +
+                "Inside, we both know what's been going on (going on)\n" +
+                "We know the game and we're gonna play it\n" +
+                "I just wanna tell you how I'm feeling\n" +
+                "Gotta make you understand\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n\n" +
+                "Now add after your name: /ivebeenrickrolled";
+    }
+
     @GetMapping()
     public String greeting(){
-        return "Regedingdong!";
+        return "Ringedingdong! \n Add your name to the url and be surprised.";
     }
 }
